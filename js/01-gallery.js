931ee28757990a377fa8cc557ddcb4787a4e4171
Serhii Karskyi <serhiipraktic7@gmail.com>
2022-11-20 15:26:18 +0000

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+	div: document.querySelector(".gallery"),
+};
+
+// refs.div.addEventListener("click", greateGalleryCardMarcup);
+
+
+function greateGalleryCardMarcup(gallary) {
+    const galleryToAdd = [];
+	gallary.map(({ preview, original, description }) => {
+        const marcup = `<div class='gallery__item'><a class='gallary__link' href='${original}'><img class='gallary__image' src='${preview}' data-source='${original}' alt='${description}'/></a></div>`;
+    galleryToAdd.push(marcup);
+    });
+    console.log(galleryToAdd)
+    return refs.div.insertAdjacentHTML('beforeend', galleryToAdd.join(''));
+}
+
+greateGalleryCardMarcup(galleryItems);
+
